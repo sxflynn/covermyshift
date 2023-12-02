853fc47699ec2ef94cd6ede53ad2c6fb6b6864ab
Stephen Flynn <stephenflynn@me.com>
2023-12-02 17:16:25 -0500

    updated example models in the vue dashboard

--- vue/src/views/DashboardView.vue
+++ vue/src/views/DashboardView.vue
@@ -1,26 +1,31 @@
 <template>
     <div></div>
-<!-- 
+    <!-- 
     Contains components: CoverForm, ListShifts -->
 
     <!-- Fake JSON -->
-const shift = {
-    isCovered : true,
+    const shift = {
+    shiftId : 1,
+    covered : true,
     shiftOwnerId : 1,
-    shiftVolunteerId : null,
+    shiftOwnerName : "Steve",
     shiftDate : "2023-12-01",
     companyId : 1
+    }
+
+const request = {
+    employeeId : 1,
+    employeeName : "Steve",
+    date : "2023-12-01"
 }
 
-const employee = {
+    const employee = {
     employeeId : 1,
     employeeName : "Steve",
     username : "user",
     email : "user@user.com",
     companyId : 1
-}
-
-
+    }
 </template>
 <script></script>
 <style></style>
