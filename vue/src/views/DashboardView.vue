36bb0db0ff55fbf8b2ae8aef03ebf1618276fafb
Stephen Flynn <stephenflynn@me.com>
2023-12-02 15:31:06 -0500

    added todos and blank components

---
+++ vue/src/views/DashboardView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div></div>
+<!-- 
+    Contains components: CoverForm, ListShifts -->
+
+    <!-- Fake JSON -->
+const shift = {
+    isCovered : true,
+    shiftOwnerId : 1,
+    shiftVolunteerId : null,
+    shiftDate : "2023-12-01",
+    companyId : 1
+}
+
+const employee = {
+    employeeId : 1,
+    employeeName : "Steve",
+    username : "user",
+    email : "user@user.com",
+    companyId : 1
+}
+
+
+</template>
+<script></script>
+<style></style>
