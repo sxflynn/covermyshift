f33318b7081abce60754b8606f59c9a13ca863f3
Niko Tatakis <nikotatakis@gmail.com>
2023-12-05 17:17:32 -0500

    imported sample data table into homeview to test with our data

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,46 +1,106 @@
 <template>
-  <v-app>
-    <v-container>
-      <v-data-table :headers="headers" :items="items" class="elevation-1">
-        <template v-slot:top>
-          <v-toolbar flat>
-            <v-toolbar-title>My Data Table</v-toolbar-title>
-            <v-divider class="mx-4" inset vertical></v-divider>
-            <v-spacer></v-spacer>
-            <v-text-field
-              v-model="search"
-              label="Search"
-              single-line
-              hide-details
-            ></v-text-field>
-          </v-toolbar>
-        </template>
-      </v-data-table>
-    </v-container>
-  </v-app>
+  <v-card flat>
+    <v-card-title class="d-flex align-center pe-2">
+      <v-icon icon="mdi-video-input-component"></v-icon> &nbsp;
+      Find a Graphics Card
+
+      <v-spacer></v-spacer>
+
+      <v-text-field
+        v-model="search"
+        prepend-inner-icon="mdi-magnify"
+        density="compact"
+        label="Search"
+        single-line
+        flat
+        hide-details
+        variant="solo-filled"
+      ></v-text-field>
+    </v-card-title>
+
+    <v-divider></v-divider>
+    <v-data-table v-model:search="search" :items="items">
+      <template v-slot:header.stock>
+        <div class="text-end">Stock</div>
+      </template>
+
+      <template v-slot:item.image="{ item }">
+        <v-card class="my-2" elevation="2" rounded>
+          <v-img
+            :src="`https://cdn.vuetifyjs.com/docs/images/graphics/gpus/${item.image}`"
+            height="64"
+            cover
+          ></v-img>
+        </v-card>
+      </template>
+
+      <template v-slot:item.rating="{ item }">
+        <v-rating
+          :model-value="item.rating"
+          color="orange-darken-2"
+          density="compact"
+          size="small"
+          readonly
+        ></v-rating>
+      </template>
+
+      <template v-slot:item.stock="{ item }">
+        <div class="text-end">
+          <v-chip
+            :color="item.stock ? 'green' : 'red'"
+            :text="item.stock ? 'In stock' : 'Out of stock'"
+            class="text-uppercase"
+            label
+            size="small"
+          ></v-chip>
+        </div>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
-
 <script>
-export default {
-  data() {
-    return {
-      search: "",
-      headers: [
-        { text: "ID", value: "id" },
-        { text: "Name", value: "name" },
-        { text: "Age", value: "age" },
-        // Add more headers as needed
-      ],
-      items: [
-        { id: 1, name: "John Doe", age: 25 },
-        { id: 2, name: "Jane Doe", age: 30 },
-        // Add more rows as needed
-      ],
-    };
-  },
-};
+  export default {
+    data () {
+      return {
+        search: '',
+        items: [
+          {
+            name: 'Nebula GTX 3080',
+            image: '1.png',
+            price: 699.99,
+            rating: 5,
+            stock: true,
+          },
+          {
+            name: 'Galaxy RTX 3080',
+            image: '2.png',
+            price: 799.99,
+            rating: 4,
+            stock: false,
+          },
+          {
+            name: 'Orion RX 6800 XT',
+            image: '3.png',
+            price: 649.99,
+            rating: 3,
+            stock: true,
+          },
+          {
+            name: 'Vortex RTX 3090',
+            image: '4.png',
+            price: 1499.99,
+            rating: 4,
+            stock: true,
+          },
+          {
+            name: 'Cosmos GTX 1660 Super',
+            image: '5.png',
+            price: 299.99,
+            rating: 4,
+            stock: false,
+          },
+        ],
+      }
+    },
+  }
 </script>
-
-<style>
-/* Add your custom styles here */
-</style>
