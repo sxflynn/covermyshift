268309944f209da4076e92a6a1ba87e85c8bce85
Stephen Flynn <stephenflynn@me.com>
2023-12-05 17:13:12 -0500

    made some front end changes

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,18 +3,21 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO users (username, password_hash, role) VALUES ('user1', 'user1', 'ROLE_USER');
+
 INSERT INTO employee (employee_name, username, email)
-VALUES ('Steve C.', 'user', 'steve@te.com');
+VALUES ('Steve C.', 'user', 'steve@te.com'),
+('Rachelle R.', 'user1', 'rachelle@te.com');
 
 INSERT INTO request (employee_id, date, is_emergency, is_pending, is_covered, is_approved, message)
 VALUES (1, '2023-12-01 00:00:00', false, true, false, false, 'My message'),
 (1, '2023-12-02 00:00:00', false, true, false, false, 'My message'),
-(1, '2024-12-02 00:00:00', false, true, false, false, 'My message');
+(2, '2024-12-02 00:00:00', false, true, false, false, 'My message');
 
-INSERT INTO Shift (is_covered, shift_owner_id, start_time, end_time)
+INSERT INTO Shift (is_covered, shift_owner_id, start_time, end_time, shift_volunteer_id)
 VALUES
-(FALSE, 1, '2022-12-05 08:00:00', '2022-12-05 16:00:00'),
-(TRUE, 1, '2023-12-06 08:00:00', '2023-12-06 16:00:00'),
-(FALSE, 1, '2024-12-07 08:00:00', '2024-12-07 16:00:00');
+(FALSE, 1, '2022-12-05 08:00:00', '2022-12-05 16:00:00', null),
+(TRUE, 1, '2023-12-16 08:00:00', '2023-12-16 16:00:00',2),
+(FALSE, 2, '2024-12-07 08:00:00', '2024-12-07 16:00:00', null);
 
 COMMIT TRANSACTION;
