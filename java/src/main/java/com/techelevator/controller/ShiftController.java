0ed0c03d0ac8cc99b6fb57e921d82725acb72311
Stephen Flynn <stephenflynn@me.com>
2023-12-05 15:44:52 -0500

    response entity

--- java/src/main/java/com/techelevator/controller/ShiftController.java
+++ java/src/main/java/com/techelevator/controller/ShiftController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,8 +21,12 @@ public class ShiftController {
 
     private final String API_BASE_SHIFT_URL = "/shift";
     @RequestMapping(path = API_BASE_SHIFT_URL + "/all", method = RequestMethod.GET)
-    public List<Shift> getAllShifts(){
-        return shiftDao.getAllShifts();
+    public ResponseEntity<List<Shift>> getAllShifts(){
+        List<Shift> allShiftList = shiftDao.getAllShifts();
+        if (allShiftList.isEmpty()){
+            return ResponseEntity.noContent().build(); // returns 204 no content
+        }
+        return ResponseEntity.ok(allShiftList);
     }
 
     @RequestMapping(path = API_BASE_SHIFT_URL + "/current", method = RequestMethod.GET)
