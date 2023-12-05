4455195b3254d9e55d575316ceb8d2fb03eaaef5
Anisa Mohamud <anisashuceeb1@gmail.com>
2023-12-05 10:42:27 -0500

    update the database and ShiftDao

--- java/src/main/java/com/techelevator/dao/ShiftDao.java
+++ java/src/main/java/com/techelevator/dao/ShiftDao.java
@@ -1,10 +1,25 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Employee;
 import com.techelevator.model.Shift;
 
 import java.util.List;
 
 public interface ShiftDao {
+
+    public List<Shift> getAllCurrentShifts();
+
+    public Shift updateShifts(Shift shift);
+
+    public List<Shift> getAllShiftsByEmployee(Employee employee);
+
+    public List<Shift> getAllUncoveredShifts();
+
+
+
+
     public List<Shift> getAllShifts();
+
+
 
 }
