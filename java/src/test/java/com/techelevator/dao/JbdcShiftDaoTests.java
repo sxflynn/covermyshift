5b77ad3cab4ea8d2d14c9cdf4441ebb22583dac9
Stephen Flynn <stephenflynn@me.com>
2023-12-07 12:36:57 -0500

    added more tests for DAO

--- java/src/test/java/com/techelevator/dao/JbdcShiftDaoTests.java
+++ java/src/test/java/com/techelevator/dao/JbdcShiftDaoTests.java
@@ -52,33 +52,43 @@ public class JbdcShiftDaoTests extends BaseDaoTests {
     );
     private final Shift SHIFT_4_SAME_DAY = new Shift(
             4,
-            false,
+            true,
             2,
             "Rachelle R.",
             null,
             null,
-            LocalDateTime.parse("2023-12-08T08:00:00"),
-            LocalDateTime.parse("2023-12-08T09:00:00")
+            LocalDateTime.parse("2025-12-08T08:00:00"),
+            LocalDateTime.parse("2025-12-08T09:00:00")
     );
     private final Shift SHIFT_5_SAME_DAY = new Shift(
             5,
-            false,
+            true,
             2,
             "Rachelle R.",
             null,
             null,
-            LocalDateTime.parse("2023-12-08T09:00:00"),
-            LocalDateTime.parse("2023-12-08T10:00:00")
+            LocalDateTime.parse("2025-12-08T09:00:00"),
+            LocalDateTime.parse("2025-12-08T10:00:00")
     );
     private final Shift SHIFT_6_SAME_DAY = new Shift(
             6,
-            false,
+            true,
             2,
             "Rachelle R.",
             null,
             null,
-            LocalDateTime.parse("2023-12-08T10:00:00"),
-            LocalDateTime.parse("2023-12-08T11:00:00")
+            LocalDateTime.parse("2025-12-08T10:00:00"),
+            LocalDateTime.parse("2025-12-08T11:00:00")
+    );
+    private final Shift SHIFT_7_SAME_DAY_STEVE = new Shift(
+            7,
+            true,
+            1,
+            "Steve C.",
+            null,
+            null,
+            LocalDateTime.parse("2025-12-08T10:00:00"),
+            LocalDateTime.parse("2025-12-08T11:00:00")
     );
 
 
@@ -157,8 +167,36 @@ public class JbdcShiftDaoTests extends BaseDaoTests {
     @Test
     public void get_all_shifts_by_employeeid_and_date_returns_correct_shifts(){
         Shift testShift = SHIFT_3;
-        List<Shift> realList = dao.getAllShiftsByEmployeeIdAndDate(2,LocalDate.parse("2024-12-07"));
-        assertShiftsMatch(testShift,realList.get(0));
+        List<Shift> realList = dao.getAllShiftsByEmployeeIdAndDate(2,LocalDate.parse("2025-12-08"));
+        Assert.assertEquals(3,realList.size());
+        assertShiftsMatch(SHIFT_4_SAME_DAY,realList.get(0));
+        assertShiftsMatch(SHIFT_5_SAME_DAY,realList.get(1));
+        assertShiftsMatch(SHIFT_6_SAME_DAY,realList.get(2));
+    }
+
+    @Test
+    public void get_all_shifts_by_request_id_returns_correct_requests(){
+        int requestId = 4; // Rachelle's request
+        List <Shift> realShifts = dao.getAllShiftsByRequestId(4);
+        Assert.assertEquals(3,realShifts.size());
+        assertShiftsMatch(SHIFT_4_SAME_DAY,realShifts.get(0));
+        assertShiftsMatch(SHIFT_5_SAME_DAY,realShifts.get(1));
+        assertShiftsMatch(SHIFT_6_SAME_DAY,realShifts.get(2));
+    }
+
+    @Test
+    public void update_list_of_shifts_to_uncovered_by_request_id_returns_correct_number(){
+       int requestId = 4;
+        int updateCount = dao.updateListOfShiftsToUncoveredByRequestId(requestId);
+        Assert.assertEquals("Expected update count to be 3, but was " + updateCount,3,updateCount);
+        Shift realShift4 = dao.getShiftByShiftId(4);
+        Shift realShift5 = dao.getShiftByShiftId(5);
+        Shift realShift6 = dao.getShiftByShiftId(6);
+        Shift realShift7 = dao.getShiftByShiftId(7);
+        Assert.assertFalse("Expected isCovered to be false",realShift4.isCovered());
+        Assert.assertFalse("Expected isCovered to be false",realShift5.isCovered());
+        Assert.assertFalse("Expected isCovered to be false",realShift6.isCovered());
+        Assert.assertTrue("Expected isCovered to be true",realShift7.isCovered());
     }
 
     @Test
