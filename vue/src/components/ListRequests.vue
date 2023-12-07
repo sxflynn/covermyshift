bb881e8b79b1a7920ffb3c17c1721990e981b94a
Gregg Sanders <takosuper1@gmail.com>
2023-12-07 13:49:23 -0500

    experimenting with home page and vuetify

--- vue/src/components/ListRequests.vue
+++ vue/src/components/ListRequests.vue
@@ -1,81 +1,120 @@
 <template>
   <v-card flat>
     <v-card-title class="d-flex align-center pe-2">
-      <v-icon icon="mdi-video-input-component"></v-icon> &nbsp;
-      List of Requests
+      <v-icon icon="mdi-video-input-component"></v-icon> &nbsp; List of Requests
 
       <v-spacer></v-spacer>
 
-      <v-text-field v-model="search" prepend-inner-icon="mdi-magnify" density="compact" label="Search" single-line flat
-        hide-details variant="solo-filled"></v-text-field>
+      <v-text-field
+        v-model="search"
+        prepend-inner-icon="mdi-magnify"
+        density="compact"
+        label="Search"
+        single-line
+        flat
+        hide-details
+        variant="solo-filled"
+      ></v-text-field>
     </v-card-title>
 
     <v-divider></v-divider>
     <!-- TODO: Add custom headers using the headers prop -->
     <!-- TODO: Customize the items-per-page -->
-    <v-data-table v-model:search="search" :items="reversedRequests" :items-per-page="1000">
-
+    <v-data-table
+      v-model:search="search"
+      :items="reversedRequests"
+      :items-per-page="1000"
+    >
       <template v-slot:item.requestId="{ item }">
         <div class="text-end">
-          <v-chip :text="item.requestId"
-            class="text-lowercase" label size="large"></v-chip>
+          <v-chip
+            :text="item.requestId"
+            class="text-lowercase"
+            label
+            size="large"
+          ></v-chip>
         </div>
       </template>
 
       <template v-slot:item.employeeId="{ item }">
         <div class="text-end">
-          <v-chip :text="item.employeeId"
-            class="text-lowercase" label size="large"></v-chip>
+          <v-chip
+            :text="item.employeeId"
+            class="text-lowercase"
+            label
+            size="large"
+          ></v-chip>
         </div>
       </template>
 
       <template v-slot:item.employeeName="{ item }">
         <div class="text-end">
-          <v-chip :text="item.employeeName"
-          label size="large"></v-chip>
+          <v-chip :text="item.employeeName" label size="large"></v-chip>
         </div>
       </template>
 
-
       <template v-slot:item.date="{ item }">
         <div class="text-end">
-          <v-chip :text="item.date"
-            class="text-lowercase" label size="large"></v-chip>
+          <v-chip
+            :text="item.date"
+            class="text-lowercase"
+            label
+            size="large"
+          ></v-chip>
         </div>
       </template>
 
       <template v-slot:item.message="{ item }">
         <div class="text-end">
-          <v-chip :text="item.message"
-          label size="large"></v-chip>
+          <v-chip :text="item.message" label size="large"></v-chip>
         </div>
       </template>
 
       <template v-slot:item.emergency="{ item }">
         <div class="text-end">
-          <v-chip :color="item.emergency ? 'red' : 'blue'" :text="item.emergency ? 'Emergency' : 'Vacation'"
-            class="text-uppercase" label size="large"></v-chip>
+          <v-chip
+            :color="item.emergency ? 'red' : 'blue'"
+            :text="item.emergency ? 'Emergency' : 'Vacation'"
+            class="text-uppercase"
+            label
+            size="large"
+          ></v-chip>
         </div>
       </template>
 
       <template v-slot:item.approved="{ item }">
         <div class="text-end">
-          <v-chip :color="item.approved ? 'green' : 'red'" :text="item.approved ? 'Approved' : 'Unapproved'"
-            class="text-uppercase" label size="large"></v-chip>
+          <v-chip
+            :color="item.approved ? 'green' : 'red'"
+            :text="item.approved ? 'Approved' : 'Unapproved'"
+            class="text-uppercase"
+            label
+            size="large"
+          ></v-chip>
         </div>
       </template>
-      
+
       <template v-slot:item.covered="{ item }">
         <div class="text-end">
-          <v-chip :color="item.covered ? 'green' : 'red'" :text="item.covered ? 'Covered' : 'Uncovered'"
-            class="text-uppercase" label size="large"></v-chip>
+          <v-chip
+            :color="item.covered ? 'green' : 'red'"
+            :text="item.covered ? 'Covered' : 'Uncovered'"
+            class="text-uppercase"
+            label
+            size="large"
+          ></v-chip>
         </div>
       </template>
 
       <template v-slot:item.pending="{ item }">
         <div class="text-end">
-          <v-chip :color="item.pending ? 'red' : 'green'" :text="item.pending ? 'Pending' : 'Finalized'"
-            class="text-uppercase" label size="large"></v-chip>
+          <v-chip
+            :color="item.pending ? 'red' : 'green'"
+            :text="item.pending ? 'Pending' : 'Finalized'"
+            class="text-uppercase"
+            label
+            size="large"
+          ></v-chip>
         </div>
       </template>
     </v-data-table>
@@ -86,42 +125,44 @@
 export default {
   data() {
     return {
-      headers:[
-      { text: 'Request ID', value: 'requestId', align: 'start' },
-      { text: 'Employee ID', value: 'employeeId', align: 'start' },
-      { text: 'Employee Name', value: 'employeeName', align: 'start' },
-      { text: 'Date', value: 'date', align: 'start' },
-      { text: 'Message', value: 'message', align: 'start' },
-      { text: 'Emergency', value: 'emergency', align: 'center' },
-      { text: 'Covered', value: 'covered', align: 'center' },
-      { text: 'Approved', value: 'approved', align: 'center' },
-      { text: 'Pending', value: 'pending', align: 'center' },
+      headers: [
+        { text: "Request ID", value: "requestId", align: "start" },
+        { text: "Employee ID", value: "employeeId", align: "start" },
+        { text: "Employee Name", value: "employeeName", align: "start" },
+        { text: "Date", value: "date", align: "start" },
+        { text: "Message", value: "message", align: "start" },
+        { text: "Emergency", value: "emergency", align: "center" },
+        { text: "Covered", value: "covered", align: "center" },
+        { text: "Approved", value: "approved", align: "center" },
+        { text: "Pending", value: "pending", align: "center" },
       ],
-      search: '',
+      search: "",
 
       requests: [
         {
-          "requestId": 1,
-          "employeeId": 1,
-          "employeeName": "Steve C.",
-          "date": "2023-12-01",
-          "message": "hello",
-          "emergency": false,
-          "covered": false,
-          "approved": false,
-          "pending": true
+          requestId: 1,
+          employeeId: 1,
+          employeeName: "Steve C.",
+          date: "2023-12-01",
+          message: "hello",
+          emergency: false,
+          covered: false,
+          approved: false,
+          pending: true,
         },
       ],
-    }
-  }, 
-  computed:{
-    reversedRequests(){
+    };
+  },
+  computed: {
+    reversedRequests() {
       return [...this.$store.state.listReqArr].reverse();
-    }
-  }
-}
-
-
-
+    },
+  },
+};
 </script>
-<style></style>
+<style scoped>
+th > div > span {
+  color: blueviolet;
+  background-color: black;
+}
+</style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,8 +1,65 @@
 <template>
-  Welcome!
-  </template>
-  <script>
-
+  <v-app>
+    <v-container class="hometitle">
+      <v-row id="hometitle1">
+        <v-card><h1>Cover</h1></v-card>
+      </v-row>
+    </v-container>
+    <v-container>
+      <v-row id="hometitle2">
+        <v-card><h1>My</h1></v-card>
+      </v-row>
+    </v-container>
+    <v-container>
+      <v-row id="hometitle3">
+        <v-card><h1>Shift</h1></v-card>
+      </v-row>
+    </v-container>
+    <v-container>
+      <v-row>
+        <v-card
+          id="home-description"
+          title="Description"
+          text="..."
+          variant="tonal"
+        ></v-card>
+      </v-row>
+      <v-container class="register-area">
+        <v-card id="register-button"><v-btn>Register</v-btn></v-card>
+        <v-card><v-text-field clearable label="Email"></v-text-field></v-card>
+      </v-container>
+    </v-container>
+    <v-container class="login">
+      <v-row id="general-login-btn">
+        <v-card><v-btn>Login</v-btn></v-card>
+      </v-row>
+      <v-col id="teacher-login-btn">
+        <v-card><v-btn>Teacher</v-btn></v-card>
+      </v-col>
+      <v-col id="admin-login-btn">
+        <v-card><v-btn>Admin</v-btn></v-card>
+      </v-col>
+    </v-container>
+    <container>
+      <v-data-table :items="items"></v-data-table>
+    </container>
+  </v-app>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      items: [
+        {
+          name: "African Elephant",
+          species: "Loxodonta africana",
+          diet: "Herbivore",
+          habitat: "Savanna, Forests",
+        },
+      ],
+    };
+  },
+};
 </script>
-<style>
-</style>
+
