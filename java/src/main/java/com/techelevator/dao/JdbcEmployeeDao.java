a71442463271aef91cc5a14e60b8b497b759915b
Niko Tatakis <nikotatakis@gmail.com>
2023-12-08 15:17:44 -0500

    request controller for claiming shift, approving shift, employeeDao method

--- java/src/main/java/com/techelevator/dao/JdbcEmployeeDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcEmployeeDao.java
@@ -1,22 +1,38 @@
 package com.techelevator.dao;
 
+import com.techelevator.exception.DaoException;
 import com.techelevator.model.User;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 public class JdbcEmployeeDao implements EmployeeDao {
 
     private final JdbcTemplate jdbcTemplate;
 
     //TODO complete this select string
-    private final String ALL_EMPLOYEE_COLUMNS = "SELECT ";
+    private final String ALL_EMPLOYEE_COLUMNS = "SELECT * FROM employee";
 
     public JdbcEmployeeDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    //TODO implement this method
+    //TODO implement this method (errorHandling not done) no test
     @Override
     public int getEmployeeIdFromUser(User user) {
+        String sql = "SELECT employee_id FROM employee \n" +
+                "WHERE username = ?;";
+        try {
+            SqlRowSet result = jdbcTemplate.queryForRowSet(sql, user.getUsername());
+            while (result.next()) {
+                int employeeId = result.getInt("employee_id");
+                return employeeId;
+            }
+
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+
+        }
         return 0;
     }
 
