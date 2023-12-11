32457469a476e3c33a9a3892b36e633ab28e7f32
Stephen Flynn <stephenflynn@me.com>
2023-12-11 15:05:49 -0500

    deleted forgot password

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -20,8 +20,7 @@
 
             <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
               Password
-              <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer" target="_blank">
-                Forgot login password?</a>
+
             </div>
 
             <v-text-field v-model="user.password" :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
