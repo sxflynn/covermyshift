e6250dd78f609f2f24fe0507fb313385fdbb7061
Stephen Flynn <stephenflynn@me.com>
2023-12-12 20:59:16 -0500

    EmailJS sending service now activated and working

--- vue/src/components/CoverForm.vue
+++ vue/src/components/CoverForm.vue
@@ -6,34 +6,19 @@
           <v-col cols="12" md="8">
             <v-row>
               <v-col cols="12">
-                <v-textarea
-                  label="Reason for request (optional)"
-                  v-model="coverReq.employeeMessage"
-                  outlined
-                  dense
-                ></v-textarea>
-                <v-checkbox
-                  label="Is this an emergency?"
-                  v-model="coverReq.emergency"
-                ></v-checkbox>
+                <v-textarea label="Reason for request (optional)" v-model="coverReq.employeeMessage" outlined
+                  dense></v-textarea>
+                <v-checkbox label="Is this an emergency?" v-model="coverReq.emergency"></v-checkbox>
               </v-col>
               <v-col cols="12">
                 <v-btn class="mr-4" color="primary" type="submit">Submit</v-btn>
-                <v-btn color="error" type="button" v-on:click="cancelForm"
-                  >Cancel</v-btn
-                >
+                <v-btn color="error" type="button" v-on:click="cancelForm">Cancel</v-btn>
               </v-col>
             </v-row>
           </v-col>
 
           <v-col cols="12" md="4">
-            <v-date-picker
-              elevation="5"
-              show-adjacent-months
-              color="primary"
-              :rules="dateRules"
-              v-model="coverReq.date"
-            >
+            <v-date-picker elevation="5" show-adjacent-months color="primary" :rules="dateRules" v-model="coverReq.date">
             </v-date-picker>
           </v-col>
         </v-row>
@@ -43,9 +28,6 @@
 </template>
 
 <script>
-// import emailjs from '@emailjs/browser';
-
-
 export default {
   data() {
     return {
@@ -65,38 +47,16 @@ export default {
           if (value) return true;
           return "A date is required";
         },
-      ],
+      ]
     };
   },
   methods: {
-    sendRequestEmail() {
-
-      const fromName = this.$store.state.employeeName; // Example: this.userName or this.form.name
-      const emergency = this.coverReq.emergency ? "This is an emergency request!" : ""
-      const message = "The employee, " + this.$store.state.employeeName + "has requested time off for the day of " +
-        this.date +
-        ". They have included the following message: " +
-        this.coverReq.message + ". " + emergency; // Example: this.form.message
-      const replyTo = this.$store.state.loggedInEmployee.email; // Example: this.form.email
-      const templateParams = {
-        from_name: fromName,
-        message: message,
-        reply_to: replyTo
-      };
-
-
-      emailjs.send('service_xsowi2y', 'template_r7geovx', templateParams)
-        .then(function (response) {
-          console.log('SUCCESS!', response.status, response.text);
-        }, function (error) {
-          console.log('FAILED...', error);
-        });
-    },
     submitForm() {
       console.log("this.coverReq prior to dispatch is ", this.coverReq);
       this.$store
         .dispatch("createNewRequest", this.coverReq)
         .then((response) => {
+          console.log("this is the .then after the dispatch")
           this.$router.push({ name: "dashboard" });
         })
         .catch((error) => {
