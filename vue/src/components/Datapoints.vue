03889bd253a32faa3b3b0e476c3a6221b504f09b
Stephen Flynn <stephenflynn@me.com>
2023-12-13 14:08:58 -0500

    added datapoints component with basic chart

---
+++ vue/src/components/Datapoints.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
+    </div>
+</template>
+<script>
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'Rachelle', 'John', 'Laura' ],
+        datasets: [ { data: [2, 3, 8] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+
+</script>
+<style></style>
