0633dfcef9387cb15653496515a64b6f427c6d70
Gregg Sanders <takosuper1@gmail.com>
2023-12-13 09:47:43 -0500

    fixed merge

--- vue/src/views/DashboardView.vue
+++ vue/src/views/DashboardView.vue
@@ -1,17 +1,21 @@
 <template>
-<<<<<<< HEAD
-  <v-card>
-    <v-tabs v-model="tab" bg-color="cyan" class="v-slide-group-item--active">
-=======
   <v-container v-if="!isUser" id="data">
     <v-row justify="center">
       <v-col cols="12" sm="3">
-        <v-card height="8rem"  elevation="3" color="orange" class="text-h3" title="Emergency Requests">
-          <v-card-text class="text-h3 text-center">{{ this.$store.state.listReqArr.length }}</v-card-text>
+        <v-card
+          height="8rem"
+          elevation="3"
+          color="orange"
+          class="text-h3"
+          title="Emergency Requests"
+        >
+          <v-card-text class="text-h3 text-center">{{
+            this.$store.state.listReqArr.length
+          }}</v-card-text>
         </v-card>
       </v-col>
       <v-col elevation="3" cols="12" sm="3">
-        <v-card height="8rem"  color="yellow" title="Unclaimed Shifts">
+        <v-card height="8rem" color="yellow" title="Unclaimed Shifts">
           <v-card-text class="text-h3 text-center">0</v-card-text>
         </v-card>
       </v-col>
@@ -21,7 +25,7 @@
         </v-card>
       </v-col>
       <v-col elevation="3" cols="12" sm="3">
-        <v-card height="8rem"  color = "red" title="Rejected Requests">
+        <v-card height="8rem" color="red" title="Rejected Requests">
           <v-card-text class="text-h3 text-center">2</v-card-text>
         </v-card>
       </v-col>
@@ -31,37 +35,22 @@
   <v-card color="light-blue-lighten-5">
     <v-tabs v-model="tab" bg-color="light-blue-lighten-4">
       <v-tab v-if="!isUser" value="allrequests">All Requests</v-tab>
->>>>>>> 210dbab9f59fa15fe8843be8738ef6874d9d31c5
       <v-tab value="shifts">Shifts to Cover</v-tab>
       <v-tab v-if="isUser" value="myshifts">My Shifts</v-tab>
       <v-tab v-if="isUser" value="myrequests">My Requests</v-tab>
     </v-tabs>
-<<<<<<< HEAD
-
-=======
->>>>>>> 210dbab9f59fa15fe8843be8738ef6874d9d31c5
     <v-card-text>
       <v-window v-model="tab">
         <v-window-item v-if="!isUser" value="allrequests">
           <list-requests />
         </v-window-item>
         <v-window-item value="shifts">
-<<<<<<< HEAD
-=======
-
->>>>>>> 210dbab9f59fa15fe8843be8738ef6874d9d31c5
           <list-shifts-to-cover />
         </v-window-item>
 
         <v-window-item value="myshifts">
           <list-shifts />
-<<<<<<< HEAD
-=======
-
->>>>>>> 210dbab9f59fa15fe8843be8738ef6874d9d31c5
         </v-window-item>
-
-
 
         <v-window-item value="myrequests">
           <my-requests />
@@ -81,17 +70,10 @@ export default {
     ListShifts,
     ListRequests,
     ListShiftsToCover,
-<<<<<<< HEAD
+    MyRequests,
   },
 
   data() {
-=======
-    MyRequests
-  },
-
-  data() {
-
->>>>>>> 210dbab9f59fa15fe8843be8738ef6874d9d31c5
     return {
       tab: null,
       showShifts: true,
@@ -112,32 +94,14 @@ export default {
   },
   computed: {
     isUser() {
-<<<<<<< HEAD
-      console.log(
-        "My authority is ",
-        this.$store.state.user.authorities[0].name
-      );
       return this.$store.state.user.authorities[0].name === "ROLE_USER";
-=======
-      return this.$store.state.user.authorities[0].name === 'ROLE_USER';
->>>>>>> 210dbab9f59fa15fe8843be8738ef6874d9d31c5
     },
   },
 };
 </script>
-<<<<<<< HEAD
-<style scoped>
-=======
 <style>
->>>>>>> 210dbab9f59fa15fe8843be8738ef6874d9d31c5
 .v-sheet--offset {
   top: -24px;
   position: relative;
 }
-<<<<<<< HEAD
-.v-slide-group-item--active {
-  background-color: orangered;
-}
-=======
->>>>>>> 210dbab9f59fa15fe8843be8738ef6874d9d31c5
 </style>
