732ef3bf9b7b84205f83b4aa3eeec50d1f581a66
Stephen Flynn <stephenflynn@me.com>
2023-12-14 11:05:51 -0500

    removed shift volunteer column from shifts to cover component

--- vue/src/components/ListShiftsToCover.vue
+++ vue/src/components/ListShiftsToCover.vue
@@ -112,7 +112,7 @@ export default {
         // { title: 'Shift Owner ID', key: 'shiftOwnerId', align: 'start' },
         { title: 'Shift Owner', key: 'shiftOwnerName', align: 'center' },
         // { title: 'Shift Volunteer ID', key: 'shiftVolunteerId', align: 'start' },
-        { title: 'Shift Volunteer', key: 'shiftVolunteerName', align: 'center' },
+        // { title: 'Shift Volunteer', key: 'shiftVolunteerName', align: 'center' },
         { title: 'Date/Time', key: 'startTime', align: 'center' },
         // { title: 'End Time', key: 'endTime', align: 'center' },
         { title: 'Covered', key: 'covered', align: 'center' },
